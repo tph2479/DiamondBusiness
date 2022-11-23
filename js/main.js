c168db0b4e2e49049f4f9a7e347445e00a61328a
tph2479 <108340077+tph2479@users.noreply.github.com>
2022-11-23 13:53:41 +0700

    load sp

--- js/main.js
+++ js/main.js
@@ -364,50 +364,51 @@ var products = [
 
 
 function Pagination() {
-    var product_items = "";
+    var product_items_1 = "";
     for(var i = 0; i < products.length; i++) {
-        var product_item = `
-    <div class="col-xl-3 col-lg-4 col-md-6">
-        <div class="product-item">
-            <div class="position-relative bg-light overflow-hidden">
-            <img
-                class="img-fluid w-100"
-                src="img/${products[i].img}"
-                alt=""
-            />
-            <div
-                class="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3"
-            >
-                New
-            </div>
-            </div>
-            <div class="text-center p-4">
-            <a class="d-block h5 mb-2" href="">${products[i].name}</a>
-            <span class="text-primary me-1">${products[i].price}</span>
-            </div>
-            <div class="d-flex border-top">
-            <small class="w-50 text-center border-end py-2">
-                <a class="text-body" href=""
-                ><i class="fa fa-eye text-primary me-2"></i>View
-                detail</a
-                >
-            </small>
-            <small class="w-50 text-center py-2">
-                <a class="text-body" href="" onclick="addItemToCart()"
-                ><i class="fa fa-shopping-bag text-primary me-2"></i>Add
-                to cart</a
-                >
-            </small>
-            </div>
-        </div>
-    </div>`;
-
-        product_items += product_item;
+        if(products[i].type == "kim cương") {
+            var product_item = `
+            <div class="col-xl-3 col-lg-4 col-md-6">
+                <div class="product-item">
+                    <div class="position-relative bg-light overflow-hidden">
+                    <img
+                        class="img-fluid w-100"
+                        src="img/${products[i].img}"
+                        alt=""
+                    />
+                    <div
+                        class="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3"
+                    >
+                        New
+                    </div>
+                    </div>
+                    <div class="text-center p-4">
+                    <a class="d-block h5 mb-2" href="">${products[i].name}</a>
+                    <span class="text-primary me-1">${products[i].price}</span>
+                    </div>
+                    <div class="d-flex border-top">
+                    <small class="w-50 text-center border-end py-2">
+                        <a class="text-body" href=""
+                        ><i class="fa fa-eye text-primary me-2"></i>View
+                        detail</a
+                        >
+                    </small>
+                    <small class="w-50 text-center py-2">
+                        <a class="text-body" href="" onclick="addItemToCart()"
+                        ><i class="fa fa-shopping-bag text-primary me-2"></i>Add
+                        to cart</a
+                        >
+                    </small>
+                    </div>
+                </div>
+            </div>`;
+            product_items_1 += product_item;
+        }
     }
 
     document.getElementById("tab-1").innerHTML = `
     <div class="row g-4">
-        ${product_items}
+        ${product_items_1}
         <div class="col-12 text-center">
             <a class="btn btn-primary rounded-pill py-3 px-5" href="">Browse More Products</a>
         </div>
